620ed4c9ee19228101639c7b3ddfd435e8b4a565
Anatoliy Serputov <aserputov@myseneca.ca>
2021-11-21 22:46:45 -0500

    Initial Commit

---
+++ SpaceApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpaceApp
+//
+//  Created by Anatoliy Serputov on 2021-11-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
